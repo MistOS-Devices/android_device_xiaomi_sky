6052aee3c86e8e046d6050481071952c7809bc3c
dhanush281 <dhanushvozh345@gmail.com>
2025-01-09 08:34:13 +0530

    Welcome to mist os

--- lineage_sky.mk
+++ lineage_sky.mk
@@ -12,10 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Configure full_base_telephony.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit common Bliss configurations
-$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
-
-$(call inherit-product, vendor/bliss/config/BoardConfigReservedSize.mk)
+# Inherit common MistOs configurations
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configurations
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
@@ -23,11 +21,18 @@ $(call inherit-product, $(DEVICE_PATH)/device.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/sky/sky-vendor.mk)
 
-BLISS_BUILDTYPE := OFFICIAL
+# MistOs Flags
+MIST_BUILD_TYPE := OFFICIAL
+MISTOS_MAINTAINER := Dhanush
+TARGET_ENABLE_BLUR := false
+PRODUCT_NO_CAMERA := true
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := false
+WITH_GMS := true
+TARGET_DEFAULT_PIXEL_LAUNCHER := false
 
 ## Device identifier
 PRODUCT_DEVICE := sky
-PRODUCT_NAME := bliss_sky
+PRODUCT_NAME := lineage_sky
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 23076RN4BI
 PRODUCT_MANUFACTURER := Xiaomi
