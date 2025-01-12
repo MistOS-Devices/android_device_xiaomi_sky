864e159648169a410f75a3e99c06ed307621e189
dhanush281 <dhanushvozh345@gmail.com>
2025-01-12 18:38:33 +0000

    amend:bringup changes

--- device.mk
+++ device.mk
@@ -388,7 +388,7 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH) \
-    hardware/bliss/interfaces/power-libperfmgr \
+    hardware/lineage/interfaces/power-libperfmgr \
     hardware/google \
     hardware/google/interfaces \
     hardware/google/pixel \
@@ -521,6 +521,3 @@ PRODUCT_PACKAGES += \
 
 # BCR
 $(call inherit-product, vendor/bcr/bcr.mk)
-
-# Dex
-PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
