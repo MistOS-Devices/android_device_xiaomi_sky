aba8d74f7cc0a3e2ad3891b65be2c668872efc48
dhanush281 <dhanushvozh345@gmail.com>
2025-01-29 09:33:43 +0530

    sky: Use partition reserved size config

--- lineage_sky.mk
+++ lineage_sky.mk
@@ -15,6 +15,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit common MistOs configurations
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+$(call inherit-product, vendor/lineage/config/BoardConfigReservedSize.mk)
+
 # Inherit device configurations
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
 
